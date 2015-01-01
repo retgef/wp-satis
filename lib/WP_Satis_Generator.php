d4740ab61bb8cfc58b25277d839194fc0877afd7
Brian Fegter <brian.fegter@amcnetworks.com>
2015-01-01 04:02:18 -0500

    Added docs

--- lib/WP_Satis_Generator.php
+++ lib/WP_Satis_Generator.php
@@ -1,18 +1,37 @@
 <?php
+/**
+ * Satis config generator for WordPress packages
+ */
 class WP_Satis_Generator{
 
+	/**
+	 * Packages index
+	 * @var array
+	 */
 	var $packages = array();
-	var $json = array();
 
+	/**
+	 * Setter
+	 * @param string $k key
+	 * @param string $v value
+	 */
 	function set($k, $v){
 		$this->$k = $v;
 	}
 
+	/**
+	 * Build controller
+	 * @return string JSON
+	 */
 	function build(){
 		$this->generate_packages();
 		return $this->render_json();
 	}
 
+	/**
+	 * Get included WordPress versions limited by user
+	 * @return mixed array/(bool)false
+	 */
 	function get_included_versions(){
 		if($included = $this->included_versions){
 			$parts = explode(',', $included);
@@ -26,6 +45,10 @@ class WP_Satis_Generator{
 		}
 	}
 
+	/**
+	 * Generate packages for tags and branches
+	 * @return mixed array/(bool)false
+	 */
 	function generate_packages(){
 		$tags = $this->get_tags_list();
 
@@ -57,6 +80,10 @@ class WP_Satis_Generator{
 		}
 	}
 
+	/**
+	 * List tags from WordPress/WordPress git repo
+	 * @return array versions
+	 */
 	function get_tags_list(){
 		$list = `git ls-remote --tags git@github.com:WordPress/WordPress.git | cut -d '/' -f 3`;
 		if($list){
@@ -66,6 +93,10 @@ class WP_Satis_Generator{
 		return $tags;
 	}
 
+	/**
+	 * List branches from WordPress/WordPress git repo
+	 * @return array branch names
+	 */
 	function get_branches_list(){
 		$list = `git ls-remote --heads git@github.com:WordPress/WordPress.git | cut -d '/' -f 3`;
 		if($list){
@@ -75,6 +106,11 @@ class WP_Satis_Generator{
 		return $branches;
 	}
 
+	/**
+	 * Add package to index
+	 * @param string $type [tag|branch]
+	 * @param string $ver  version name/number
+	 */
 	function add_package($type, $ver){
 		$version = $type === 'tag' ? $ver : "dev-$ver";
 		$package = array(
@@ -123,6 +159,11 @@ class WP_Satis_Generator{
 		$this->packages[] = $package;
 	}
 
+	/**
+	 * Map PHP version dependency to WordPress version
+	 * @param  string $ver tag/branch
+	 * @return string      version
+	 */
 	function get_php_dependency($ver){
 		switch ($ver) {
 			case $ver >= 3.2:
@@ -141,6 +182,10 @@ class WP_Satis_Generator{
 		return $php;
 	}
 
+	/**
+	 * Render Satis JSON
+	 * @return string JSON
+	 */
 	function render_json(){
 		$satis = array(
 			'name' => $this->name,
